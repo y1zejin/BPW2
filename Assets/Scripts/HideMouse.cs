f19e125b16fb842eb7771dcc54641109d3cefeb4
y1zejin <143601243+y1zejin@users.noreply.github.com>
2024-03-28 18:14:59 +0100

    main menu fix

--- Assets/Scripts/HideMouse.cs
+++ Assets/Scripts/HideMouse.cs
@@ -7,12 +7,6 @@ public class HideMouse : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Cursor.visible = false;
     }
 }
